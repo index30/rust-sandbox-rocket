8cd922175353b5b16b0bf5d59c02793bf9b0197f
yusei <realthingshakes25@gmail.com>
2024-11-28 10:51:36 +0900

    add: ルーティングの追加

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,14 @@ fn index() -> &'static str { // リクエストハンドラー
     "Hello, world!" // TODO: HTMLを呼び出すには？
 }
 
+#[get("/echo")] // ルーティング先のディレクトリ構造(/sampleにマウントしているので、パスはsample/echoとなる)
+fn sample() -> &'static str { // リクエストハンドラー
+    "Echo Sample"
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+        .mount("/", routes![index]) // ルートディレクトリへのマウント
+        .mount("/sample", routes![sample]) // sampleディレクトリへのマウント
 }
