26862f027bd1412801f601895c546c2a67475a0a
yusei <realthingshakes25@gmail.com>
2025-01-06 17:10:40 +0900

    [add] テストコードの実装

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ fn rocket() -> _ {
     let rocket = rocket::build()
         .mount("/", routes![index]) // ルートディレクトリへのマウント
         .mount("/sample", routes![sample]) // sampleディレクトリへのマウント
-        .attach(Template::fairing());
+        .attach(Template::fairing()); // テンプレート機能の有効化
     
     // コンフィグまわり
     let figment = rocket.figment();
@@ -34,4 +34,26 @@ fn rocket() -> _ {
     println!("{:?}", config);
 
     rocket
+}
+
+// テストコード
+#[cfg(test)]
+mod test {
+    use super::rocket;
+    use rocket::uri;
+    use rocket::routes;
+    use rocket::http::{ContentType, Status};
+    use rocket::local::blocking::Client;
+
+    #[test]
+    fn sample() {
+        let client = Client::tracked(rocket()).expect("valid rocket instance");
+        println!("{}",uri!(super::sample)); // sampleは/sampleにマウントしているが、get内のパスを参照して、/echoと表示される
+        let mut response = client.get("/sample/echo").dispatch(); //直接指定すると通るが・・・
+
+        assert_eq!(response.status(), Status::Ok);
+        assert_eq!(response.content_type(), Some(ContentType::Plain));
+        assert!(response.headers().get_one("X-Content-Type-Options").is_some());
+        assert_eq!(response.into_string().unwrap(), "Echo Sample");
+    }
 }
